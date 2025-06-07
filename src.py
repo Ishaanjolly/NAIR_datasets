60838af5ac3acda98b090a23b1f80a5c12efe0d6
IshaanJolly <ijolly1997@gmail.com>
2025-06-07 15:57:45 +0530

    feat: update src

--- src.py
+++ src.py
@@ -1,19 +1,24 @@
 """loads hendrycks_math dataset and saves train and test splits to json files"""
+from huggingface_hub import login
 from datasets import load_dataset
 
-types = ['algebra', 'counting_and_probability', 'geometry', 'intermediate_algebra', 'number_theory', 'prealgebra', 'precalculus']
+if __name__ == "__main__":
+    # Replace 'your_token_here' with your actual Hugging Face token
+    login(token = 'your token here')
 
-for t in types: 
-    dataset = load_dataset("EleutherAI/hendrycks_math", t) 
-    dataset['train'].to_json('data/hendrycks_math_' + t + '_train.json')
-    dataset['test'].to_json('data/hendrycks_math_' + t + '_test.json')
-    
-dataset = load_dataset("commonsense_qa")
-dataset['train'].to_json('data/common_sense_train.json')
-dataset['test'].to_json('data/common_sense_test.json')
-dataset['validation'].to_json('data/common_sense_validation.json')
+    types = ['algebra', 'counting_and_probability', 'geometry', 'intermediate_algebra', 'number_theory', 'prealgebra', 'precalculus']
 
-for t in ["main", "socratic"]: 
-    dataset = load_dataset("gsm8k", t)
-    dataset['train'].to_json('gsmk8_'+ t + '_train.jsonl')
-    dataset['test'].to_json('gsmk8_'+ t + '_train.jsonl')
+    for t in types: 
+        dataset = load_dataset("EleutherAI/hendrycks_math", t) 
+        dataset['train'].to_json('data/hendrycks_math_' + t + '_train.json')
+        dataset['test'].to_json('data/hendrycks_math_' + t + '_test.json')
+        
+    dataset = load_dataset("commonsense_qa")
+    dataset['train'].to_json('data/common_sense_train.json')
+    dataset['test'].to_json('data/common_sense_test.json')
+    dataset['validation'].to_json('data/common_sense_validation.json')
+
+    for t in ["main", "socratic"]: 
+        dataset = load_dataset("gsm8k", t)
+        dataset['train'].to_json('gsmk8_'+ t + '_train.jsonl')
+        dataset['test'].to_json('gsmk8_'+ t + '_train.jsonl')
