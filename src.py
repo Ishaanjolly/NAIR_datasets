9310d0cc3072bb84b92c5a55dd94641efc6a3a1f
IshaanJolly <ijolly1997@gmail.com>
2025-06-07 15:55:35 +0530

    feat: code for the data

--- src.py
+++ src.py
@@ -8,3 +8,12 @@ for t in types:
     dataset['train'].to_json('data/hendrycks_math_' + t + '_train.json')
     dataset['test'].to_json('data/hendrycks_math_' + t + '_test.json')
     
+dataset = load_dataset("commonsense_qa")
+dataset['train'].to_json('data/common_sense_train.json')
+dataset['test'].to_json('data/common_sense_test.json')
+dataset['validation'].to_json('data/common_sense_validation.json')
+
+for t in ["main", "socratic"]: 
+    dataset = load_dataset("gsm8k", t)
+    dataset['train'].to_json('gsmk8_'+ t + '_train.jsonl')
+    dataset['test'].to_json('gsmk8_'+ t + '_train.jsonl')
